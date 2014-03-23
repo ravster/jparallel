5b79a9c9911de910122b88274dead2e8f43aeac8
Ravi Desai <ravi@ravidesai.com>
2014-03-23 11:55:29 -0400

    Convert to class to benefit from a persistent thread-pool

--- lib/jparallel.rb
+++ lib/jparallel.rb
@@ -1,50 +1,44 @@
 require "bundler/setup"
 require "thread/pool"
 
-module Jparallel
-  def self.processor_count
-    java.lang.Runtime.getRuntime.availableProcessors
+class Jparallel
+  def initialize poolsize
+    @pool = Thread.pool poolsize
   end
 
-  def self.map (input_array, poolsize = processor_count, &block)
+  def map (input_array, &block)
     output_array = Array.new input_array.length
-    thread_pool = Thread.pool poolsize
 
     input_array.each_with_index do |item, index|
-      thread_pool.process do
+      @pool.process do
         output_array[index] = yield(item)
       end
     end
 
-    thread_pool.shutdown # blocks till all the work is done.
     output_array
   end
 
-  def self.map_with_index (input_array, poolsize = processor_count, &block)
+  def map_with_index (input_array, &block)
     output_array = Array.new input_array.size
-    thread_pool = Thread.pool poolsize
 
     input_array.each_with_index do |item, index|
-      thread_pool.process do
+      @pool.process do
         output_array[index] = yield(item, index)
       end
     end
 
-    thread_pool.shutdown
     output_array
   end
 
-  def self.hashmap (input_hash, poolsize = processor_count, &block)
+  def hashmap (input_hash, &block)
     output_hash = {}
-    thread_pool = Thread.pool poolsize
 
     input_hash.each_pair do |key, value|
-      thread_pool.process do
+      @pool.process do
         output_hash[key] = yield(key, value)
       end
     end
 
-    thread_pool.shutdown
     output_hash
   end
 end
--- spec/jparallel_spec.rb
+++ spec/jparallel_spec.rb
@@ -2,16 +2,14 @@ require "bundler/setup"
 require "jparallel"
 
 describe Jparallel do
+  let(:jp) { Jparallel.new 6 }
+
   describe "mapping functions" do
     let(:input_array) { (10..15).to_a }
 
     describe "::map" do
       let(:multiply_by_2) do
-        Jparallel.map(input_array) { |x| x * 2 }
-      end
-
-      let(:manual_poolsize) do
-        Jparallel.map(input_array, 3) { |x| x * 2 }
+        jp.map(input_array) { |x| x * 2 }
       end
 
       let(:regular_array_map) do
@@ -21,15 +19,11 @@ describe Jparallel do
       it "mimics Array#map" do
         expect(multiply_by_2).to eql(regular_array_map)
       end
-
-      it "works with a given poolsize" do
-        expect(manual_poolsize).to eql(regular_array_map)
-      end
     end
 
     describe "::map_with_index" do
       let(:jp_map_with_index) do
-        Jparallel.map_with_index(input_array) do |x, index|
+        jp.map_with_index(input_array) do |x, index|
           "#{x} is at index #{index}"
         end
       end
@@ -48,7 +42,7 @@ describe Jparallel do
     let(:input_hash) { {a: 1, b: 2, c: 3} }
 
     let(:hash_by_2) do
-      Jparallel.hashmap(input_hash) { |key, val| val * 2 }
+        jp.hashmap(input_hash) { |key, val| val * 2 }
     end
 
     it "mimics Hash[ Hash#map ]" do
